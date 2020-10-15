8500bbcd0051f59222ec7d7e891f7e8436fbf4ca
Ana Lam <alam@barnard.edu>
2020-10-15 14:53:36 -0400

    get scripts.js working

--- scripts.js
+++ scripts.js
@@ -21,15 +21,15 @@
 
 // Intialize the map as the variable "map"
 // This also hides the + / - zoom controls.
-// const map = L.map("mapdiv", { zoomControl: false });
+const map = L.map("mapdiv", { zoomControl: false });
 
 // Set a center point and zoom level for it:
-// const vingtNeufRueDUlm = L.latLng([48.843495, 2.344888]);
-// const zoomLevel = 17;
+const vingtNeufRueDUlm = L.latLng([48.843495, 2.344888]);
+const zoomLevel = 17;
 
 // Now set the view of the map and add a tile layer:
-// map.setView(vingtNeufRueDUlm, zoomLevel);
-// L.tileLayer.provider("Stamen.Watercolor").addTo(map);
+map.setView(vingtNeufRueDUlm, zoomLevel);
+L.tileLayer.provider("Stamen.Watercolor").addTo(map);
 
 // If you prefer a different tile layer, see your options here:
 // https://leaflet-extras.github.io/leaflet-providers/preview/
@@ -38,14 +38,14 @@
 // "OpenStreetMap.Mapnik" or "Stamen.Toner"
 
 // Add a marker for where we're at:
-// const vingtNeufMarker = L.marker(vingtNeufRueDUlm).addTo(map);
+const vingtNeufMarker = L.marker(vingtNeufRueDUlm).addTo(map);
 
 // Now add a popup to it:
-// vingtNeufMarker.bindPopup("<h3>Hello from 29 rue d’Ulm!</h3>");
+vingtNeufMarker.bindPopup("<h3>Hello from 29 rue d’Ulm!</h3>");
 
 // Use Markdown, instead:
-// const md = markdownit({html: true}).use(markdownitFootnote);
-// vingtNeufMarker.bindPopup(md.render("### Hello from 29 rue d’Ulm and the [NYU/PSL Workshop](https://wp.nyu.edu/nyupslgeo/workshop/)!"));
+const md = markdownit({html: true}).use(markdownitFootnote);
+vingtNeufMarker.bindPopup(md.render("### Hello from 29 rue d’Ulm and the [NYU/PSL Workshop](https://wp.nyu.edu/nyupslgeo/workshop/)!"));
 
 // Use d3 to parse the places.csv csv file.
 //
@@ -58,7 +58,7 @@
 // That gives us a new object that we call "list." It's not an Array, but it
 // behaves somewhat like one. The last member, however, is the header row.
 
-/*
+
 d3.csv("places.csv", data => {
   return {
     nom: data.nom,
@@ -78,32 +78,32 @@ d3.csv("places.csv", data => {
       let icon;
       switch (place.type) {
       case "batuniv":
-        color = "#cc0000";
+        // color = "#cc0000";
         icon = "graduation-cap";
         break;
       case "restmex":
-        color = "#00cc00";
+        // color = "#00cc00";
         icon = "utensils";
         break;
       case "librairie":
-        color = "#0000cc";
+        // color = "#0000cc";
         icon = "book";
         break;
       }
       // And then we use the other properties to make add a circle marker to the map.
-      L.circleMarker([place.latitude, place.longitude],
-        { fillColor: color, color: color }
-      ).bindPopup(`<h3><a href="${place.lien}">${place.nom}</a></h3>`).addTo(map);
+      // L.circleMarker([place.latitude, place.longitude],
+        // { fillColor: color, color: color }
+      // ).bindPopup(`<h3><a href="${place.lien}">${place.nom}</a></h3>`).addTo(map);
       // Alternatively, we can use icons from font-awesome.
-      // L.marker([place.latitude, place.longitude],
-      //   { icon: L.divIcon(
-      //     { html: `<i style="color: ${color}" class="fa fa-${icon}"></i>`, iconSize: [30, 30] }
-      //   )}
-      // ).bindTooltip(place.nom).addTo(map);
+      L.marker([place.latitude, place.longitude],
+        { icon: L.divIcon(
+          { html: `<i style="color: ${color}" class="fa fa-${icon}"></i>`, iconSize: [30, 30] }
+        )}
+      ).bindTooltip(place.nom).addTo(map);
     }
   });
 });
-*/
+
 
 // Alternatively, we can contact a server and ask data from it.
 //
@@ -112,11 +112,10 @@ d3.csv("places.csv", data => {
 
 // Shift into async mode and put it on the map:
 
-/*
 
-$.getJSON(velibStationUrl, stationData => {
+// $.getJSON(velibStationUrl, stationData => {
   // Create a layer group for the stations
-  const velibStationsLayer = L.layerGroup();
+  // const velibStationsLayer = L.layerGroup();
 
   // stationData has an array inside of it called "records" that
   // holds each station's data. Within each record, there's a "fields"
@@ -124,44 +123,45 @@ $.getJSON(velibStationUrl, stationData => {
   // and "lon."
 
   // iterate over the records array
-  stationData.records.forEach( record => {
+  // stationData.records.forEach( record => {
     // build an L.circleMarker to add to the velibStationMarkers array.
-    const velibCircle = L.circleMarker([record.fields.lat, record.fields.lon], {
-      radius: 15,
-      color: "#eeeeee", // outline or stroke color
-      weight: 2, // outline or stroke width
-      fillColor: "#666666",
-      fillOpacity: 0.5
-    }).bindPopup(`<h3>${record.fields.name}</h3>`);
-    velibStationsLayer.addLayer(velibCircle);
-  });
+    // const velibCircle = L.circleMarker([record.fields.lat, record.fields.lon], {
+      // radius: 15,
+      // color: "#eeeeee", // outline or stroke color
+      // weight: 2, // outline or stroke width
+      // fillColor: "#666666",
+      // fillOpacity: 0.5
+    // }).bindPopup(`<h3>${record.fields.name}</h3>`);
+    // velibStationsLayer.addLayer(velibCircle);
+  // });
   // And add it to the map
-  velibStationsLayer.addTo(map);
+  // velibStationsLayer.addTo(map);
 
   // Now convert the layer to GeoJSON so we can work on it with Turf.
-  const velibStationsGeoJSON = velibStationsLayer.toGeoJSON();
+  // const velibStationsGeoJSON = velibStationsLayer.toGeoJSON();
   // In turf, let's buffer the points and then dissolve them together
-  const bufferedStations = turf.dissolve(
-    turf.buffer(
-      velibStationsGeoJSON, 400, { units: "meters" }
-    )
-  );
-  L.geoJSON(bufferedStations, {
-    style() {
-      return {
-        color: "#ff0000",
-        weight: 5,
-        fillOpacity: 0.0
-      };
-    }
-  }).addTo(map);
-}); // close $.getJSON()
+  // const bufferedStations = turf.dissolve(
+    // turf.buffer(
+      // velibStationsGeoJSON, 400, { units: "meters" }
+    // )
+  // );
+  // L.geoJSON(bufferedStations, {
+    // style() {
+      // return {
+        // color: "#ff0000",
+        // weight: 5,
+        // fillOpacity: 0.0
+      // };
+    // }
+  // }).addTo(map);
+// }); // close $.getJSON()
 
-*/
+
+
 
 // Or, we can use $.getJSON() on our own file of the Parisian Arrondisements.
 
-/*
+
 $.getJSON("paris_arr.geojson", geodata => {
   L.geoJSON(geodata, {
     style() {
@@ -173,16 +173,16 @@ $.getJSON("paris_arr.geojson", geodata => {
     }
   }).addTo(map);
 });
-*/
+
 
 // Use jQuery to manipulate the html elements.
 // Change the card header:
-// $("#card-header-text").html("<strong>Workshop à rue d’Ulm</strong>");
+$("#card-header-text").html("<strong>Workshop à rue d’Ulm</strong>");
 
-// Change the card body to the body.md file: 
+// Change the card body to the body.md file:
 
-// $.ajax({ url: "body.md",
-//   success(bodyMarkdown) {
-//     $("#outlet-card-body").html(md.render(bodyMarkdown)); 
-//   } 
-// });
+$.ajax({ url: "body.md",
+  success(bodyMarkdown) {
+    $("#outlet-card-body").html(md.render(bodyMarkdown));
+  }
+});
